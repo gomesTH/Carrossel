e83b9ae59029565fec90e5f04d3a824ffa18aeb3
Thiago Gomes <thiago.gpixel@gmail.com>
2021-08-12 15:00:07 -0300

    Carrossel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import {useEffect, useState, useRef} from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const carrossel = useRef(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/static/shoes.json')
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  const handleLeftClick = (e) =>{
+    e.preventDefault();
+    carrossel.current.scrollLeft -= carrossel.current.offsetWidth;
+  };
+
+
+  const handleRightClick = (e) =>{
+    e.preventDefault();
+    carrossel.current.scrollLeft += carrossel.current.offsetWidth;
+    
+  }
+
+  if (!data || !data.length) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <div className="logo">
+          <img src="/static/images/super-shoes.png" alt="Super Shoes Logo" />
+        </div>
+        <div className="carrossel" ref={carrossel}>
+          {data.map((item)=>{
+            const {id, name, price, oldPrice, image}=item;
+            return (
+          <div className="item" key={id}>
+            <div className="image">
+              <img src={image} alt={name}/>
+            </div>
+            <div className="info">
+            <span className="name">{name}</span>
+              <span className="oldPrice"> U$ {oldPrice} </span>
+              <span className="price"> U$ {price} </span>
+            </div>
+          </div>
+          );
+        })}
+        </div>
+        <div className="buttons">
+          <button onClick={handleLeftClick}>
+            <img src="/static/images/216151_right_chevron_icon.png" alt="Scroll Left"/>
+          </button>
+          <button onClick={handleRightClick}>
+            <img src="/static/images/216151_right_chevron_icon.png" alt="Scroll Left"/>
+          </button>
+        </div>
+      </div>
   );
 }
 
